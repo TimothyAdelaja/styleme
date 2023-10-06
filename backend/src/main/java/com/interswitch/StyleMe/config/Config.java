05b48be00212264516f4d0b3a394bb5077e51ec6
MafSholl <marvesholl1@gmail.com>
2023-10-06 14:20:58 +0100

    refactoring import for cors config

--- backend/src/main/java/com/interswitch/StyleMe/config/Config.java
+++ backend/src/main/java/com/interswitch/StyleMe/config/Config.java
@@ -8,7 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 public class Config {
