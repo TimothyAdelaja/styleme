1cc94c908eb33d31614a08ba81c1a7442bd1f066
MafSholl <marvesholl1@gmail.com>
2023-10-06 17:14:49 +0100

    replaced frontend app

---
+++ frontend-nw/config.jsx
@@ -0,0 +1,8 @@
+const config = {
+    REACT_APP_GOOGLE_API_TOKEN: '1035677996605-dmrq4enfm0ih38fqjt9t5i2gmuolp9ri.apps.googleusercontent.com',
+    REACT_APP_SIGNUP_URL: 'http://localhost:8080/api/v1/auth/signup',
+    REACT_APP_GOOGLE_SIGNUP_URL: 'http://localhost:8080/api/v1/auth/googlesignup',
+    REACT_APP_LOGIN_URL: 'http://localhost:8080/api/v1/auth/login'
+  };
+  
+  export default config;
