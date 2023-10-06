d5d492a0622ffcab04b43e06bc67e978e989ccb6
Kadiri Kelvin <kevthedev007@gmail.com>
2023-10-06 15:26:21 +0100

    fix - config.jsx added

---
+++ frontend/config.jsx
@@ -0,0 +1,7 @@
+const config = {
+    REACT_APP_GOOGLE_API_TOKEN: '1035677996605-dmrq4enfm0ih38fqjt9t5i2gmuolp9ri.apps.googleusercontent.com',
+    REACT_APP_SIGNUP_URL: 'http://localhost:8080/api/v1/auth/signup',
+    REACT_APP_GOOGLE_SIGNUP_URL: 'http://localhost:8080/api/v1/auth/googlesignup'
+  };
+  
+  export default config;
