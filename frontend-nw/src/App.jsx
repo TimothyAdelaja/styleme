4e7521ef38b325b6b654f2c38106eca8d22de574
Joseph Enubiak <enubiakjoseph@gmail.com>
2023-10-10 12:21:37 +0100

    solved

--- frontend-nw/src/App.jsx
+++ frontend-nw/src/App.jsx
@@ -6,20 +6,28 @@ import OtpInput from "./pages/otpInput/otpInput";
 import SignUpPage from "./pages/SignUp/SignUpPage";
 import SignIn from "./pages/loginPage/signin";
 import ResetPassword from "./components/login/reset";
+import WardrobeWork from "./pages/wardrobe/WardrobeWork";
+import WardrobeSunday from "./pages/wardrobe/WardrobeSunday";
+import WardrobeNative from "./pages/wardrobe/WardrobeNative";
+import WardrobeParty from "./pages/wardrobe/WardrobeParty";
 
 function App() {
-	return (
-		<div>
-			<Routes>
-				<Route path="/" element={<LandingPage />} />
-				<Route path="/wardrobe" element={<Wardrobe />} />
-				<Route path="/signup" element={<SignUpPage />} />
-				<Route path="/otp-input" element={<OtpInput />} />
-				<Route path="/login" element={<SignIn />} />
-				<Route path="/reset-password" element={<ResetPassword />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/wardrobe" element={<Wardrobe />} />
+        <Route path="work/:id" element={<WardrobeWork />} />
+        <Route path="sunday/:sunday" element={<WardrobeSunday />} />
+        <Route path="native/:native" element={<WardrobeNative />} />
+        <Route path="party/:party" element={<WardrobeParty />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/otp-input" element={<OtpInput />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend-nw/src/pages/wardrobe/WardrobeNative.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+
+const WardrobeNative = () => {
+  const location = useLocation();
+  const { native } = useParams();
+  const [data, setData] = useState([]);
+  console.log("Data ", data);
+  useEffect(() => {
+    setData(location.state.nativeOutfits);
+  }, []);
+  return (
+    <section id="wardrobeInner">
+      <div className="container mx-auto flex space-x-20 p-6">
+        <h2 className="text-5xl  font-bold text-blue-400 mt-10">
+          {native} closet{" "}
+        </h2>
+        {data.length > 0 ? (
+          data.map((item) => (
+            <div
+              className="flex flex-col justify-start items-center mt-20"
+              key={item.driveId}
+            >
+              <img
+                className="w-[200px] h-[200px]"
+                src={item?.url}
+                alt={item.itemCategory}
+              />
+              <p className="text-xl font-bold font-helvetica mt-6">
+                {item.itemCategory}
+              </p>
+            </div>
+          ))
+        ) : (
+          <h1 className="mt-[20rem] text-5xl text-black font-bold">No data</h1>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default WardrobeNative;
